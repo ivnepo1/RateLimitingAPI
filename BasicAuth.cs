e3c765777151fcad65487079579f2f0475d7bb70
ivnepo1 <idcnepomuceno@gmail.com>
2023-05-01 16:49:49 +0800

    feat: basic auth

---
+++ BasicAuth.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+public class BasicAuthMiddleware : IMiddleware
+{
+    private const string Realm = "My Realm";
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        if (!context.Request.Headers.ContainsKey("Authorization"))
+        {
+            context.Response.Headers.Add("WWW-Authenticate", $"Basic realm=\"{Realm}\"");
+            context.Response.StatusCode = 401;
+            return;
+        }
+
+        var authHeader = AuthenticationHeaderValue.Parse(context.Request.Headers["Authorization"]);
+        var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(authHeader.Parameter));
+        var split = credentials.Split(':', 2);
+        var username = split[0];
+        var password = split[1];
+
+        if (username != "sim" || password != "corp")
+        {
+            context.Response.StatusCode = 401;
+            return;
+        }
+
+        await next(context);
+    }
+}
